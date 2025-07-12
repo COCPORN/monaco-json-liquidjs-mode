a838aa39544d7cc4db6cfe9489faa6f3fb80c3db
Lars Thomas Denstad <ungedenstad@gmail.com>
2025-07-13 01:28:00 +0200

    Adding completion to JSON

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerLiquidJSLanguage = registerLiquidJSLanguage;
+function getNestedObjectPaths(obj, parentKey = '') {
+    let keys = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const newKey = parentKey ? `${parentKey}.${key}` : key;
+            keys.push(newKey);
+            if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+                keys = keys.concat(getNestedObjectPaths(obj[key], newKey));
+            }
+        }
+    }
+    return keys;
+}
 function registerLiquidJSLanguage(monacoNs, sampleData) {
     monacoNs.languages.register({ id: 'liquid-json' });
+    const liquidKeywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
+        'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
+        'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
+        'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
+        'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
+        'contains', 'startswith', 'endswith', 'comment', 'endcomment',
+        'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
+        'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
+        'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
+        'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
+        'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
+        'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
+        'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
+        'remove_first', 'replace', 'replace_first', 'reverse', 'round',
+        'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
+        'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
+        'uniq', 'upcase', 'url_decode', 'url_encode'];
+    const dataKeys = getNestedObjectPaths(sampleData);
     monacoNs.languages.registerCompletionItemProvider('liquid-json', {
         provideCompletionItems: (model, position) => {
             const word = model.getWordUntilPosition(position);
@@ -12,43 +43,45 @@ function registerLiquidJSLanguage(monacoNs, sampleData) {
                 startColumn: word.startColumn,
                 endColumn: word.endColumn
             };
-            var autocompleteProviderItems = [];
-            var keywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
-                'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
-                'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
-                'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
-                'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
-                'contains', 'startswith', 'endswith', 'comment', 'endcomment',
-                'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
-                'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
-                'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
-                'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
-                'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
-                'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
-                'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
-                'remove_first', 'replace', 'replace_first', 'reverse', 'round',
-                'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
-                'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
-                'uniq', 'upcase', 'url_decode', 'url_encode'];
-            for (var i = 0; i < keywords.length; i++) {
-                autocompleteProviderItems.push({
-                    label: keywords[i],
-                    kind: monacoNs.languages.CompletionItemKind.Keyword,
-                    insertText: keywords[i],
-                    range: range
-                });
-            }
-            // Add sample data keys to autocomplete
-            const dataKeys = Object.keys(sampleData);
-            for (const key of dataKeys) {
-                autocompleteProviderItems.push({
+            const lineContent = model.getLineContent(position.lineNumber);
+            const textBefore = lineContent.substring(0, position.column - 1);
+            let suggestions = [];
+            // Keyword suggestions
+            suggestions.push(...liquidKeywords.map(keyword => ({
+                label: keyword,
+                kind: monacoNs.languages.CompletionItemKind.Keyword,
+                insertText: keyword,
+                range: range
+            })));
+            // Nested object suggestions
+            const liquidExpressionMatch = textBefore.match(/\{\{\s*([\w\.]*)$/);
+            if (liquidExpressionMatch) {
+                const currentPath = liquidExpressionMatch[1];
+                const parts = currentPath.split('.');
+                let keys = dataKeys;
+                if (parts.length > 1) {
+                    const parentPath = parts.slice(0, -1).join('.');
+                    keys = dataKeys.filter(k => k.startsWith(parentPath + '.') && k.split('.').length === parts.length);
+                }
+                else {
+                    keys = dataKeys.filter(k => k.split('.').length === 1);
+                }
+                suggestions.push(...keys.map(key => ({
                     label: key,
                     kind: monacoNs.languages.CompletionItemKind.Variable,
                     insertText: key,
                     range: range
-                });
+                })));
             }
-            return { suggestions: autocompleteProviderItems };
+            else {
+                suggestions.push(...dataKeys.filter(k => k.split('.').length === 1).map(key => ({
+                    label: key,
+                    kind: monacoNs.languages.CompletionItemKind.Variable,
+                    insertText: key,
+                    range: range
+                })));
+            }
+            return { suggestions: suggestions };
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
 import * as monaco from 'monaco-editor';
 
+function getNestedObjectPaths(obj: {[key: string]: any}, parentKey: string = ''): string[] {
+    let keys: string[] = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const newKey = parentKey ? `${parentKey}.${key}` : key;
+            keys.push(newKey);
+            if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+                keys = keys.concat(getNestedObjectPaths(obj[key], newKey));
+            }
+        }
+    }
+    return keys;
+}
+
 export function registerLiquidJSLanguage(monacoNs: typeof monaco, sampleData: object): void {
     monacoNs.languages.register({ id: 'liquid-json' });
+
+    const liquidKeywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
+        'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
+        'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
+        'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
+        'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
+        'contains', 'startswith', 'endswith', 'comment', 'endcomment',
+        'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
+        'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
+        'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
+        'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
+        'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
+        'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
+        'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
+        'remove_first', 'replace', 'replace_first', 'reverse', 'round',
+        'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
+        'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
+        'uniq', 'upcase', 'url_decode', 'url_encode'];
+
+    const dataKeys = getNestedObjectPaths(sampleData);
 
     monacoNs.languages.registerCompletionItemProvider('liquid-json', {
         provideCompletionItems: (model, position) => {
@@ -13,46 +47,50 @@ export function registerLiquidJSLanguage(monacoNs: typeof monaco, sampleData: ob
                 endColumn: word.endColumn
             };
 
-            var autocompleteProviderItems = [];
-            var keywords = ['assign', 'capture', 'endcapture', 'increment', 'decrement',
-                        'if', 'else', 'elsif', 'endif', 'for', 'endfor', 'break',
-                        'continue', 'limit', 'offset', 'range', 'reversed', 'cols',
-                        'case', 'endcase', 'when', 'block', 'endblock', 'true', 'false',
-                        'in', 'unless', 'endunless', 'cycle', 'tablerow', 'endtablerow',
-                        'contains', 'startswith', 'endswith', 'comment', 'endcomment',
-                        'raw', 'endraw', 'editable', 'endentitylist', 'endentityview', 'endinclude',
-                        'endmarker', 'entitylist', 'entityview', 'forloop', 'image', 'include',
-                        'marker', 'outputcache', 'plugin', 'style', 'text', 'widget',
-                        'abs', 'append', 'at_least', 'at_most', 'capitalize', 'ceil', 'compact',
-                        'concat', 'date', 'default', 'divided_by', 'downcase', 'escape',
-                        'escape_once', 'first', 'floor', 'join', 'last', 'lstrip', 'map',
-                        'minus', 'modulo', 'newline_to_br', 'plus', 'prepend', 'remove',
-                        'remove_first', 'replace', 'replace_first', 'reverse', 'round',
-                        'rstrip', 'size', 'slice', 'sort', 'sort_natural', 'split', 'strip',
-                        'strip_html', 'strip_newlines', 'times', 'truncate', 'truncatewords',
-                        'uniq', 'upcase', 'url_decode', 'url_encode'];
+            const lineContent = model.getLineContent(position.lineNumber);
+            const textBefore = lineContent.substring(0, position.column -1);
 
-            for (var i = 0; i < keywords.length; i++) {
-                autocompleteProviderItems.push({
-                    label: keywords[i],
-                    kind: monacoNs.languages.CompletionItemKind.Keyword,
-                    insertText: keywords[i],
-                    range: range
-                });
-            }
+            let suggestions = [];
 
-            // Add sample data keys to autocomplete
-            const dataKeys = Object.keys(sampleData);
-            for (const key of dataKeys) {
-                autocompleteProviderItems.push({
+            // Keyword suggestions
+            suggestions.push(...liquidKeywords.map(keyword => ({
+                label: keyword,
+                kind: monacoNs.languages.CompletionItemKind.Keyword,
+                insertText: keyword,
+                range: range
+            })));
+            
+            // Nested object suggestions
+            const liquidExpressionMatch = textBefore.match(/\{\{\s*([\w\.]*)$/);
+            if(liquidExpressionMatch) {
+                const currentPath = liquidExpressionMatch[1];
+                const parts = currentPath.split('.');
+                
+                let keys = dataKeys;
+                if (parts.length > 1) {
+                    const parentPath = parts.slice(0, -1).join('.');
+                    keys = dataKeys.filter(k => k.startsWith(parentPath + '.') && k.split('.').length === parts.length);
+                } else {
+                    keys = dataKeys.filter(k => k.split('.').length === 1);
+                }
+
+                suggestions.push(...keys.map(key => ({
                     label: key,
                     kind: monacoNs.languages.CompletionItemKind.Variable,
                     insertText: key,
                     range: range
-                });
+                })));
+            } else {
+                 suggestions.push(...dataKeys.filter(k => k.split('.').length === 1).map(key => ({
+                    label: key,
+                    kind: monacoNs.languages.CompletionItemKind.Variable,
+                    insertText: key,
+                    range: range
+                })));
             }
 
-            return { suggestions: autocompleteProviderItems };
+
+            return { suggestions: suggestions };
         }
     });
 }
